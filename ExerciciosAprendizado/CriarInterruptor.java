09c95e404a80591033600228903f7f72cb26101a
Enzo Gabriel <enzoddos7@gmail.com>
2023-07-19 14:23:23 -0400

    estudando

--- ExerciciosAprendizado/CriarInterruptor.java
+++ ExerciciosAprendizado/CriarInterruptor.java
@@ -2,11 +2,7 @@ package ExerciciosAprendizado;
 
 public class CriarInterruptor {
     public static void main(String[] args) {
-        Interruptor elemento = new Interruptor();
-        elemento.espelho = " Branco";
-        elemento.fio = " Normais";
-        elemento.tomada = " Padrão Brasileiro";
-        elemento.corTomada = " Branco";
+        Interruptor elemento = new Interruptor("Branco", "Normais", "Padrao Brasileiro", "Branco");
         elemento.ligar();
         elemento.estado(); 
         elemento.status();
--- ExerciciosAprendizado/Interruptor.java
+++ ExerciciosAprendizado/Interruptor.java
@@ -1,17 +1,66 @@
 package ExerciciosAprendizado;
 
 public class Interruptor {
-    String espelho;
-    boolean botao;
-    String fio;
-    String tomada;
-    String corTomada;
+   public String espelho;
+   private boolean botao;
+   public String fio;
+   public String tomada;
+   public String corTomada;
+   
+    public Interruptor(String espelho, String fio, String tomada, String corTomada) {
+    this.espelho = espelho;
+    this.fio = fio;
+    this.tomada = tomada;
+    this.corTomada = corTomada;
+}
+
+    /////////////////////////////////
+    public String getEspelho(){
+       return this.espelho;
+    }
+
+    public void setEspelho(String e){
+       this.espelho = e;
+    }
+    ///////////////////////////////
+    public boolean getBotao(){
+       return this.botao;
+    }
+
+    public void setBotao(boolean b){
+       this.botao = b;
+    }
+    ///////////////////////////////
+    public String getFio(){
+       return this.fio;
+    }
+
+    public void setFio(String f){
+       this.fio = f;
+    }
+    /////////////////////
+    public String getTomada(){
+       return this.tomada;
+    }
+
+    public void setTomada(String t){
+       this.tomada = t;
+    }
+    //////////////////////////////
+    public String getCorTomada(){
+       return this.corTomada;
+    }
+
+    public void setCorTomada(String ct){
+       this.corTomada = ct;
+    }
+    //////////////////////////////
 
     void status() {
-        System.out.println("Qual a cor do espelho?" + this.espelho);
-        System.out.println("Qual o modelo da tomada?" + this.tomada);
-        System.out.println("Qual a cor da tomada?" + this.corTomada);
-        System.out.println("Está ligado?" + this.botao);
+        System.out.println("Qual a cor do espelho?" + this.getEspelho());
+        System.out.println("Qual o modelo da tomada?" + this.getTomada());
+        System.out.println("Qual a cor da tomada?" + this.getCorTomada());
+        System.out.println("Está ligado?" + this.getBotao());
     }
 
     void estado() {
